4433b6b9315318c452cf485f378b3ce67dce238f
Rohtash <59862361+rraghav476@users.noreply.github.com>
2026-06-08 14:41:57 +0530

    Add create method to AuthenticatedSessionController

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -21,6 +21,16 @@ class AuthenticatedSessionController extends Controller
     }
 
     /**
+     * Display the login view.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function create()
+    {
+        return view('auth.login');
+    }
+
+    /**
      * Handle an incoming authentication request.
      *
      * @param  \App\Http\Requests\Auth\LoginRequest  $request
